8cbc210ed096880a941864a969689cf0ce367c05
boladss <anton.boladss@gmail.com>
2023-12-08 00:19:55 +0800

    feat: add bfs.h
    Add parameters relevant to BFS and the seed for the random function.

---
+++ bfs.h
@@ -0,0 +1,9 @@
+/*
+Note: These values are simply placeholders and may still be changed.
+*/
+
+#define   BFS_DEFAULT_QUANTUM     50      // temporary value -- (temp based on Lab 5)
+#define   NICE_FIRST_LEVEL        -20     // most negative value, at most 0 -- (temp based on CFS)
+#define   NICE_LAST_LEVEL         19      // most positive value, never less than first level
+
+#define   SEED                    140     // for random function
